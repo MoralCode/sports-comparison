a0b949f70d64b468ddd67e5e785ab874c6af0ed7
Adrian Edwards <adrian@adriancedwards.com>
2024-07-20 22:10:36 -0400

    chatGPT csv upgrades

--- sports.py
+++ sports.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
 
-# Hardcoded data
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [5, 6, 2, 3, 13, 4, 1, 2, 4, 8]
-z = [2, 3, 3, 3, 5, 7, 9, 11, 9, 10]
+# Read data from CSV file
+data = pd.read_csv('data.csv')
+
+# Extract columns
+names = data['name']
+x = data['x']
+y = data['y']
+z = data['z']
 
 # Create a new figure for plotting
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Scatter plot
-ax.scatter(x, y, z, c='r', marker='o')
+scatter = ax.scatter(x, y, z, c='r', marker='o')
 
 # Labels
 ax.set_xlabel('X Label')
@@ -21,31 +26,9 @@ ax.set_zlabel('Z Label')
 # Title
 ax.set_title('3D Scatter Plot')
 
-# Show plot
-plt.show()
- 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-# Hardcoded data
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [5, 6, 2, 3, 13, 4, 1, 2, 4, 8]
-z = [2, 3, 3, 3, 5, 7, 9, 11, 9, 10]
-
-# Create a new figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Scatter plot
-ax.scatter(x, y, z, c='r', marker='o')
-
-# Labels
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-
-# Title
-ax.set_title('3D Scatter Plot')
+# Annotate each point with its name
+for i, name in enumerate(names):
+    ax.text(x[i], y[i], z[i], name)
 
 # Show plot
 plt.show()
